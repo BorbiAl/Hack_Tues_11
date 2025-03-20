d113cc103680d25e29b6fbd1b9a0a835b405a88e
BorisAl. <boris.v.aleksandrov.2023@elsys-bg.org>
2025-03-20 21:12:31 +0200

    many updates that were needed

--- study_app/core/urls.py
+++ study_app/core/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
-from . import include
+
+app_name = 'core'
 
 urlpatterns = [
-    path('',),
+    path('', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('profile/', views.profile_view, name='profile'),
+    path('test-creation/', views.test_creation_view, name='test_creation'),
+    path('test-list/', views.test_list_view, name='test_list'),
+    path('ranking/', views.ranking_view, name='ranking'),
 ]
--- study_app/core/views.py
+++ study_app/core/views.py
@@ -1,4 +1,3 @@
-views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
