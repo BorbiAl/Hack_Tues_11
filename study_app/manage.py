d58f0337eacbc5070a27e6253fcab4824c41309f
Boris.Iordanov <boris.i.yordanov.2023@elsys-bg.org>
2025-03-20 21:14:18 +0200

    Made a little corection for the view file.

--- study_app/manage.py
+++ study_app/manage.py
@@ -11,6 +11,11 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
+            "Couldn't import Django. Ensure Django is installed in your environment. "
+            "You may need to activate your virtual environment or install Django using pip."
+        ) from exc
+    except ImportError as exc:
+        raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
