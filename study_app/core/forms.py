20dec170ef822235b94f3956f2e5dbc43a4a8f12
BorisAl. <boris.v.aleksandrov.2023@elsys-bg.org>
2025-03-21 18:37:44 +0200

    Refactor user authentication forms; implement custom user creation and login views

--- study_app/core/forms.py
+++ study_app/core/forms.py
@@ -1,12 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-class SignupForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=254, widget=forms.TextInput(attrs={'autofocus': True}))
-    password = forms.CharField(label=("Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}))
+        fields = ['username', 'password1', 'password2']
--- study_app/core/urls.py
+++ study_app/core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import home_view, login_view, signup_view, dashboard_view, profile_view, test_creation_view, test_list_view, ranking_view, generate_test_view
+from .views import home_view, CustomLoginView, signup_view, dashboard_view, profile_view, test_creation_view, test_list_view, ranking_view, generate_test_view
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('profile/', profile_view, name='profile'),
@@ -11,4 +10,5 @@ urlpatterns = [
     path('test-list/', test_list_view, name='test_list'),
     path('ranking/', ranking_view, name='ranking'),
     path('generate-test/', generate_test_view, name='generate_test'),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
