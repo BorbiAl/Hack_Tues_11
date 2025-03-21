b3d039d26ad9d3df7b7e1a0ce77dcf9df5f4cbd6
BorisAl. <boris.v.aleksandrov.2023@elsys-bg.org>
2025-03-21 13:42:12 +0200

    Update database host in settings to allow external connections

--- study_app/study_app/settings.py
+++ study_app/study_app/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'baiganio_db', 
         'USER': 'root',        
         'PASSWORD': 'YnNh768j', 
-        'HOST': '127.0.0.1',     
+        'HOST': '0.0.0.0',     
         'PORT': '3306',           
     }
 }
